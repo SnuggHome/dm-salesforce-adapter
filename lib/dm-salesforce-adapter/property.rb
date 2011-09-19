9002dacbc076b4d78126f1ae18a6d3b931658365
Frederic Jean <fred@fredjean.net>
2011-09-19 15:50:25 -0600

    Adding a Picklist property type. Retrieving the possible values from SF.

--- lib/dm-salesforce-adapter/property.rb
+++ lib/dm-salesforce-adapter/property.rb
@@ -1,7 +1,8 @@
-class SalesforceAdapter 
+class SalesforceAdapter
   module Property
   end
 end
 
 require 'dm-salesforce-adapter/property/serial'
 require 'dm-salesforce-adapter/property/boolean'
+require 'dm-salesforce-adapter/property/picklist'
